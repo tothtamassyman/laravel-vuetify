8a307b406264a2f9312917486810fdc58d112c82
tothtamassyman <toth.tamas.syman@gmail.com>
2025-01-07 18:36:58 +0100

    NotFound komponens Vissza a főoldlra gomb hozzáadása

--- resources/js/views/NotFound.vue
+++ resources/js/views/NotFound.vue
@@ -1,11 +1,14 @@
 <script setup>
-
+/** @name NotFound */
 </script>
 
 <template>
-    <div>
-        <h1>404 - Not Found</h1>
-    </div>
+    <v-container>
+        <h1>404 - Page Not Found</h1>
+        <router-link to="/">
+            <v-btn>Go to Home</v-btn>
+        </router-link>
+    </v-container>
 </template>
 
 <style scoped>
