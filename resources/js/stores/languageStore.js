2e428fccd79a6f3d93c24f80b124be8401b30a4b
tothtamassyman <toth.tamas.syman@gmail.com>
2025-01-13 10:45:05 +0100

    languageStore javítása - feleslegessé vált locales ellenőrzés elfávolítása

--- resources/js/stores/languageStore.js
+++ resources/js/stores/languageStore.js
@@ -8,14 +8,12 @@ export const useLanguageStore = defineStore('language', {
 
     actions: {
         async fetchLocales() {
-            if (Object.keys(this.locales).length === 0) {
-                try {
-                    const response = await axios.get('/locales');
-                    this.locales = response.data;
-                } catch (error) {
-                    console.error('Failed to load available locales:', error);
-                    this.locales = {en: 'English'};
-                }
+            try {
+                const response = await axios.get('/locales');
+                this.locales = response.data;
+            } catch (error) {
+                console.error('Failed to load available locales:', error);
+                this.locales = {en: 'English'};
             }
         },
     },
