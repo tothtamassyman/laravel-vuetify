2c80c18b7bfd8b3deffd8ff730dafd68341c2ba6
tothtamassyman <toth.tamas.syman@gmail.com>
2025-02-18 12:11:29 +0100

    EnhancedValidationException javítás/refaktorálás - hibakezelés hozzáadása - messages ellenőrzés javítása

--- app/Exceptions/EnhancedValidationException.php
+++ app/Exceptions/EnhancedValidationException.php
@@ -16,21 +16,25 @@ class EnhancedValidationException extends ValidationException
     /**
      * Create an error message summary from the validation errors.
      *
-     * @param Validator $validator
+     * @param  Validator  $validator
      * @return string
      */
     protected static function summarize($validator): string
     {
+        if (!$validator instanceof Validator) {
+            throw new \InvalidArgumentException('Invalid validator instance.');
+        }
+
         $messages = $validator->errors()->all();
 
-        if (!count($messages) || !is_string($messages[0])) {
+        if (empty($messages) || !isset($messages[0]) || !is_string($messages[0])) {
             return __('validation.and_more_invalid_data');
         }
 
         $message = array_shift($messages);
 
         if ($count = count($messages)) {
-            $message .= ' (' . trans_choice('validation.and_more', $count, ['count' => $count]) . ')';
+            $message .= ' ('.trans_choice('validation.and_more', $count, ['count' => $count]).')';
         }
 
         return $message;
@@ -39,7 +43,7 @@ class EnhancedValidationException extends ValidationException
     /**
      * Render the exception into an HTTP response.
      *
-     * @param Request $request
+     * @param  Request  $request
      * @return JsonResponse
      */
     public function render(Request $request): JsonResponse
