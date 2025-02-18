5d149e5c604d6c29ce13d71dd4390cb51a4372e3
tothtamassyman <toth.tamas.syman@gmail.com>
2025-02-18 12:08:20 +0100

    Reactivity Transform eltávolítása - Reactivity Transform was an experimental feature, and has been removed in the latest 3.4 release.   If you still intend to use it, it is now available via the Vue Macros plugin.   https://vue-macros.dev/features/reactivity-transform.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import vuetify from 'vite-plugin-vuetify'
+import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
     plugins: [
@@ -10,7 +10,6 @@ export default defineConfig({
             refresh: true,
         }),
         vue({
-            reactivityTransform: true,
             template: {
                 transformAssetUrls: {
                     base: null,
